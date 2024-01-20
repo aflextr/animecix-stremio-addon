7947cf179275325fa94b2cfede14b3e90fb2c966
aflextr <eyup.elitass@gmail.com>
2024-01-20 23:07:22 +0300

    test

--- header.js
+++ header.js
@@ -5,7 +5,6 @@ const header = {
 "Cache-Control": "max-age=0",
 "Connection": "keep-alive",
 "Cookie": "connect.sid=s%3APFIYlZ3_tpaStMPA7qLJRvL7gWktN2ty.LQ5k0s%2BLLAhPjOh8eXRbVig%2BVwjC3VgzxYbCnjo3kyM; animecix_net=%7B%22HttpHost%22%3A%22animecix.net%22%2C%22Protokol%22%3A%22https%22%2C%22Port%22%3A443%2C%22KulAdSifre%22%3Anull%2C%22UrlAdresi%22%3A%22%5C%2F%22%2C%22GetVeri%22%3Anull%2C%22GitOpjeId%22%3Anull%2C%22DnsAdresi%22%3A0%2C%22URL_Adresi%22%3A%22https%3A%5C%2F%5C%2Fanimecix.net%5C%2F%22%2C%22GirisIP%22%3A%22104.26.8.126%22%7D; YoncuKoruma=149.86.128.83; OsSavSec-v1=C51F2F1106C5231ED099A604F0C7292D; __Secure-YoncuSec=C51F2F1106C5231ED099A604F0C7292D; __Host-YoncuSec=C51F2F1106C5231ED099A604F0C7292D; theme=Dark",
-"Host": "animecix-dotnet.gateway.web.tr",
 "Sec-Fetch-Dest": "document",
 "Sec-Fetch-Mode": "navigate",
 "Sec-Fetch-Site": "none",
